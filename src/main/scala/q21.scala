a36aefdd7a2c66a3f6510d0146ed3348495785a1
knp069 <knp069@gmail.com>
2017-10-17 03:08:43 +0530

    q20 added

---
+++ src/main/scala/q21.scala
@@ -0,0 +1,13 @@
+object q21 {
+  def insertAt[A](e:A , n:Int , ls:List[A] ) : List[A] = {
+
+    if (ls.isEmpty || n<0 || n>ls.size) Nil
+    val (pre,post) = ls.splitAt(n)
+    pre:::List(e):::post
+  }
+
+  def main(args: Array[String]): Unit = {
+    val ls = List(1,2,3,5,6,7)
+    println("List after inserting element : " + insertAt(4,3,ls))
+  }
+}
