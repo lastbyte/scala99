3092093b1c8a04361973761092e5d80f0df46b0d
knp069 <knp069@gmail.com>
2017-10-16 07:05:29 +0530

    q14 added

---
+++ src/main/scala/q14.scala
@@ -0,0 +1,10 @@
+object q14 {
+  def duplicate[A](ls: List[A]):List[A] = {
+    ls flatMap{ e => List.make(2,e)}
+  }
+
+  def main(args: Array[String]): Unit = {
+    var ls = List(1,2,3,4,4)
+    println("Duplicated list is : " + duplicate(ls))
+  }
+}
