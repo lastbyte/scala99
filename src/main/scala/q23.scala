8c79c97ad40fa3831d0531b249f8d5f697504e18
knp069 <knp069@gmail.com>
2018-01-20 01:33:37 +0530

    q23 edited

---
+++ src/main/scala/q23.scala
@@ -0,0 +1,16 @@
+
+import scala.util.Random
+import q20.removeAt
+object q23{
+  def extractRandom[Int](n:Int, ls:List[Int]): List[Any] = {
+
+  }
+
+  def get[A](n: Int, ls: List[A]): Any = {
+
+  }
+  def main(args:Array[String]): Unit ={
+    val ls  = List(1,2,3,4,5,6,7);
+    print("3 randomly selected numbers are : " + extractRandom(3,ls))
+  }
+}
